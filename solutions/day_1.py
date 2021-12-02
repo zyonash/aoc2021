0dd33b06a24589f0f5d8e0cde0ee3d90cadc5ff5
zacharyyonash <yonash402@gmail.com>
2021-12-01 20:43:15 -0500

    Day 1, Part 2 solution

--- solutions/day_1.py
+++ solutions/day_1.py
@@ -1,10 +1,11 @@
-def main():
+def part_1():
     increases = 0
     with open("day_1_input.txt") as f:
         measurements = f.readlines()
         old_measurement = -1
         for i in measurements:
             i = i.rstrip("\n")
+            i = int(i)
             if old_measurement == -1:
                 old_measurement = i
                 print(f'{i} (N/A - no previous measurement)')
@@ -22,5 +23,21 @@ def main():
     print(f'Number of increases: {increases}')
 
 
+def part_2():
+    increases = 0
+    with open("day_1_input.txt") as f:
+        measurements = f.readlines()
+        for i in range(0, len(measurements)-1):
+            print(i)
+            if not i + 3 >= len(measurements):
+                first_sum = int(measurements[i]) + int(measurements[i+1]) + int(measurements[i+2])
+                second_sum = int(measurements[i+1]) + int(measurements[i+2]) + int(measurements[i+3])
+                if second_sum > first_sum:
+                    increases += 1
+    f.close()
+    print(f'Number of increases: {increases}')
+
+
 if __name__ == "__main__":
-    main()
+    # part_1()
+    part_2()
